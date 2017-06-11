03c4eb3595cf4dc7a30bb440d6976f3509c3419e
TanyaRan <tatyanaran@gmail.com>
2017-06-11 23:58:28 +0300

    create services

---
+++ ToysShop/ToysShop.Services/ServiceModels/CityModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToysShop.Models.SQLModels;
+using ToysShop.Models.SQLModels.Enums;
+
+namespace ToysShop.Services.ServiceModels
+{
+    public class CityModel
+    {
+        public CityModel(City city)
+        {
+            if (city != null)
+            {
+                this.Id = city.Id;
+                this.Name = city.Name;
+                this.Region = city.Region;
+            }
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Region Region { get; set; }
+
+        public IEnumerable<AddressModel> Addresses { get; set; }
+    }
+}
